49d2487239296340a1f068aa567bda6c762ba3cb
lautumn1990 <932034966@qq.com>
2017-12-06 09:09:40 +0800

    添加自动启动常用程序bat

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+start "" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\ms.lnk"
+start "" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\id.lnk"
+start "" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\gc.lnk"
+start "" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\vs.lnk"
+exit
