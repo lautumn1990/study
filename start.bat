d536943a34a0c3d84d976fa33e1b0978a89452cb
lautumn1990 <932034966@qq.com>
2017-12-20 14:18:57 +0800

    修改start.bat，如果进程存在则不启动程序

--- start.bat
+++ start.bat
@@ -1,6 +1,24 @@
-@echo off
-start "" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\id.lnk"
-start "" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\gc.lnk"
-start "" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\vs.lnk"
-start "" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\ms.lnk"
-exit
+@echo off   
+
+call:programStart "chrome.exe" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\gc.lnk"
+call:programStart "idea.exe" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\id.lnk"
+call:programStart "navicat.exe" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\ms.lnk"
+call:programStart "Code.exe" "C:\Users\Administrator.2017.V.2.12-007\Desktop\桌面快捷方式\vs.lnk"
+:: 不关闭当前cmd
+:: cmd.exe
+:: pause
+:: exit
+goto:eof 
+
+:programStart    
+tasklist -v | findstr %1 > NUL
+if ErrorLevel 1 (  
+	echo %1 进程不存在，正在启动  
+	start "" %2 
+) else (  
+	echo %1 进程已在运行 
+)    
+goto:eof  
+
+
+
