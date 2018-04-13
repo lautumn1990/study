b341f26c227243d598b47510666dc0679255a7c7
lautumn1990 <932034966@qq.com>
2018-04-13 13:10:00 +0800

    增加笔记本关闭屏幕bat

---
+++ bats/turnOffScreen.bat
@@ -0,0 +1,4 @@
+C:\Windows\System32\powercfg.exe -Change -monitor-timeout-dc 1
+C:\Windows\System32\powercfg.exe -Change -monitor-timeout-ac 1
+
+START /MIN /WAIT C:\Windows\System32\scrnsave.scr -s
