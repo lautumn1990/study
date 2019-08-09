6dc1de8698011cdd7443d034e601cbdca27433f1
lautumn1990 <932034966@qq.com>
2019-08-09 18:07:14 +0800

    20190809180713 auto commit

--- bats/gitAutoCommit/autoCommitMylogDemo.bat
+++ bats/gitAutoCommit/autoCommitMylogDemo.bat
@@ -1,4 +1,8 @@
 @echo off
 REM 需要两个参数, 一个是git目录所在位置, 一个是日志所在位置
+set GIT_PROJECT_PATH=D:\projects\github\mylog
 set LOG_PATH=%~dp0gitAutoCommitMylog.log
-call D:\projects\github\study\bats\gitAutoCommit\gitCommitAndPush.bat D:\projects\github\mylog %LOG_PATH%
+REM 命令路径
+set GIT_AUTOCOMMIT_CMD=D:\projects\github\study\bats\gitAutoCommit\gitCommitAndPush.bat
+
+call %GIT_AUTOCOMMIT_CMD% %GIT_PROJECT_PATH% %LOG_PATH%
