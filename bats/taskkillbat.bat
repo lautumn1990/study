bcd03a760ab6e7fb888846cbb9479055ea4a9a90
lautumn1990 <932034966@qq.com>
2019-09-19 19:10:03 +0800

    20190919191001 auto commit

--- bats/taskkillbat.bat
+++ bats/taskkillbat.bat
@@ -1,7 +1,7 @@
 @echo off
-taskkill /F /IM %1%.exe
+taskkill /F /IM %1.exe
 If Not %ERRORLEVEL% EQU 0 (
-    echo not found %1%.exe , just found: 
+    echo not found aaaa %1.exe, just found:
     tasklist | findstr %1%
     Pause & Exit
 )
